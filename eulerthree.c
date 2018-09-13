9b949f13eff2de8147e69f2b33a973e5c7378826
Andy <azhou4@stuy.edu>
2018-09-12 21:57:00 -0400

    fixed three five is still broken

--- eulerthree.c
+++ eulerthree.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <math.h>
 
-double main(){
-  double largest = 2;
-  double num = 600851475143;
-  while(num > 1){
-    while(fmod(num, largest) == 0){
+long main(){
+  long largest = 2;
+  long num = 600851475143;
+  while(num > largest){
+    while(num % largest == 0){
       num /= largest;
     }
+    largest++;
   }
-  printf("%f",largest);
+  printf("%li\n",largest);
   return largest;
 }
